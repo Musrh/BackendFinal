c26fb8592261cedb3d594b67e5880489d5f8b046
Musrh <musplay2023@gmail.com>
2026-05-15 13:10:30 +0100

    Remove Firebase initialization check

--- export-store.js
+++ export-store.js
@@ -8,10 +8,6 @@ import admin from "firebase-admin"
 import fs    from "fs"
 import { fileURLToPath } from "url"
 
-if (!admin.apps.length) {
-  const SERVICE_ACCOUNT = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  admin.initializeApp({ credential: admin.credential.cert(SERVICE_ACCOUNT) })
-}
 const db = admin.firestore()
 
 const serialize = (data) => {
